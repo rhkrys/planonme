13622d36cda1efd6aaedf32adece31c9cd827320
Kenneth Burling <burlingk@gmail.com>
2010-07-17 00:33:59 +0900

    I have gotten things started up.  Just to get things moving I grabbed a style sheet and a layout from another project I am working on, and modified them a bit.  I imagine that by the time this project is completed, they will look totally different.  So far I have generated the initial "site" controller, but have done little else.  I did add in a helper to make navigation links easy.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  #nav_link will create navigation links
+  def nav_link(text, controller, action)
+    link_to_unless_current text, :controller => controller, :action => action
+  end
+
+
+
 end
+
+
+#
+# Comments:
+#   I know that I will need navigation links everywhere just about,
+#   so I may as well create the helper from the start.
